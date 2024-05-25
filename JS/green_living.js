71c6bec24a78ddca6fce41b80816b72deacfbb39
Vicksata <163349315+Vicksata@users.noreply.github.com>
2024-05-25 12:03:23 +0300

    many chats support

--- JS/green_living.js
+++ JS/green_living.js
@@ -1,16 +1,41 @@
 
+
+function getChatType() {
+    const e = document.getElementById('chatType');
+    if (e) {
+        return e.value;
+    } 
+
+    return null;
+}
+
 window.onload = function(e) {
     const buttonSend = document.getElementById('SendText');
-    if (buttonSend) {
-        buttonSend.addEventListener('click', post_msg);
+    const chatType = getChatType();
+
+    if (buttonSend && chatType) {
+        if (chatType == "green_living") {
+            buttonSend.addEventListener('click', post_msg_living);
+            setInterval(load_msg_living, 1000);
+        } else if (chatType == "eco_cooking") {
+            buttonSend.addEventListener('click', post_msg_cooking);
+            setInterval(load_msg_cooking, 1000);
+        } else if (chatType == "eco_activities") {
+            buttonSend.addEventListener('click', post_msg_activities);
+            setInterval(load_msg_activities, 1000);
+        } else if (chatType == "eco_tourism") {
+            buttonSend.addEventListener('click', post_msg_tourism);
+            setInterval(load_msg_tourism, 1000);
+        }
     }
 
     var foo = getParameterByName('error');
     if (foo && foo.length > 0) {
-        document.getElementById("errorMsg").style.display = "block";
+        const errorElem = document.getElementById("errorMsg");
+        if (errorElem) {
+            errorElem.style.display = "block";
+        }
     }
-
-    setInterval(load_msg, 1000);
 }
 
 
@@ -58,8 +83,24 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-function load_msg() {
-    url ="/load_msg";
+
+function load_msg_living() {
+    load_msg_internal("/load_msg_living");
+}
+
+function load_msg_cooking() {
+    load_msg_internal("/load_msg_cooking");
+}
+
+function load_msg_activities() {
+    load_msg_internal("/load_msg_activities");
+}
+
+function load_msg_tourism() {
+    load_msg_internal("/load_msg_tourism");
+}
+
+function load_msg_internal(url) {
     fetch(url)
         .then((response) => {
             // return response.text();
@@ -73,18 +114,33 @@ function load_msg() {
         });
 }
 
-function post_msg() {
+function post_msg_living() {
+    post_msg_internal("/add_msg_living")
+}
+
+function post_msg_cooking() {
+    post_msg_internal("/add_msg_cooking")
+}
+
+function post_msg_activities() {
+    post_msg_internal("/add_msg_activities")
+}
+
+function post_msg_tourism() {
+    post_msg_internal("/add_msg_tourism")
+}
+
+function post_msg_internal(url) {
     element = document.getElementById('EnterText');
     msg = element.value;
 
-    url = "/add_msg";
     fetch(url, {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         method: "POST",
-        body: JSON.stringify({message: msg})
+        body: JSON.stringify({"message": msg})
     })
     .then((response) => {
         element.value = "";
